f9cf1dd3711526eb0fbbdadba563d5913aa819bf
Currently_Dying <gamer.2845@outlook.com>
2025-12-31 09:20:16 +0000

    Re-Commit after email verification failed

---
+++ src/main/java/net/currentlydying/makingminecraftbetter/MakingMinecraftBetter.java
@@ -0,0 +1,17 @@
+package net.currentlydying.makingminecraftbetter;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MakingMinecraftBetter implements ModInitializer {
+	public static final String MOD_ID = "makingminecraftbetter";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/currentlydying/makingminecraftbetter/MakingMinecraftBetterClient.java
@@ -0,0 +1,10 @@
+package net.currentlydying.makingminecraftbetter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MakingMinecraftBetterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
